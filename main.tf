8ee0d1a16010ed9bf7269b254b81101b1b5d82a3
Leif Eriksen <leif.eriksen.au@gmail.com>
2020-03-30 16:51:10 +1100

    init

---
+++ main.tf
@@ -0,0 +1,60 @@
+locals {
+  sql_server_name = var.sql_dw_name == "" ? format("%s-df", random_string.id.result) : var.sql_dw_name
+}
+
+resource "azurerm_sql_server" "sqldw_server" {
+  name                         = local.sql_server_name
+  resource_group_name          = var.resource_group_name
+  location                     = var.location
+  version                      = "12.0"
+  administrator_login          = var.sql_admin_user_name
+  administrator_login_password = var.sql_admin_password
+  tags                         = var.tags
+  identity                     = {
+    type = "SystemAssigned"
+  }
+}
+
+resource "azurerm_sql_database" "sqldw_db" {
+  name                             = var.db_name == "" ? format("%s-df", random_string.id.result) : var.db_name
+  resource_group_name              = var.resource_group_name
+  server_name                      = "${azurerm_sql_server.sqldw_server.name}"
+  location                         = var.location
+  edition                          = "DataWarehouse"
+  collation                        = var.db_collation
+  requested_service_objective_name = var.dwuname
+
+  tags = var.tags
+}
+
+resource "azurerm_sql_active_directory_administrator" "sqldw_aad" {
+  server_name         = "${azurerm_sql_server.sqldw_server.name}"
+  resource_group_name = var.resource_group_name
+  login               = var.aad_admin_user_name
+  tenant_id           = var.azure_tenant_id
+  object_id           = var.aad_admin_user_id
+}
+
+resource "azurerm_sql_firewall_rule" "sqldw_fw_rules" {
+  count               = length(var.whitelisted_networks)
+  name                = lookup(var.whitelisted_networks[count.index],"name")
+  resource_group_name = var.resource_group_name
+  server_name         = "${azurerm_sql_server.sqldw_server.name}"
+  start_ip_address    = lookup(var.whitelisted_networks[count.index],"start_ip")
+  end_ip_address      = lookup(var.whitelisted_networks[count.index],"end_ip")
+}
+
+resource "azurerm_sql_virtual_network_rule" "sqlvnetrule" {
+  count               = "${length(var.whitelisted_subnets)}"
+  name                = "sql-vnet-rule${count.index}"
+  resource_group_name = var.resource_group_name
+  server_name         = "${azurerm_sql_server.sqldw_server.name}"
+  subnet_id           = var.whitelisted_subnets[count.index]
+}
+
+resource "random_string" "id" {
+  length  = "8"
+  upper   = false
+  special = false
+  number  = false
+}
---
+++ variables.tf
@@ -0,0 +1,59 @@
+variable "resource_group_name" {
+  description = "The name of the resource group in which the resources will be created"
+}
+
+variable "location" {
+  description = "The location/region where the virtual network is created. Changing this forces a new resource to be created."
+  default     = "australiaeast"
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "A map of the tags to use on the resources that are deployed with this module."
+}
+
+variable "sql_dw_name" {
+  description = "SQL dw server name"
+}
+
+variable "db_name" {
+  description = "SQL database name"
+}
+
+variable "db_collation" {
+  description = "Collation of the database"
+  default = "SQL_Latin1_General_CP1_CI_AS"
+}
+
+variable "sql_admin_user_name" {
+  description = "SQL Admin username of the sqldw"
+}
+variable "sql_admin_password" {
+   description = "Password for the SQL Admin, must comply Azure policy"
+}
+variable "dwuname" {
+   description = "Capacity of the SQLDW"
+   default = "dw500c"
+}
+
+variable "aad_admin_user_id" {
+  description = "AD object id of the administrator"
+}
+
+variable "aad_admin_user_name" {
+  description = "AD object administrator name"
+}
+
+variable "azure_tenant_id" {
+  description = "Tenant_id"
+}
+
+variable "whitelisted_networks" {
+  type = list(map(string))
+  description = "A list of rules to be whitelisted from the sqldw firewall, .i.e.: [ { name = 'digitalNetwork' start_ip = '165.225.98.0' end_ip = '165.225.98.255' }]"
+}
+
+variable "whitelisted_subnets" {
+   type = list(string)
+   description = "A list of subnet ids to be whiteslisted"
+}
